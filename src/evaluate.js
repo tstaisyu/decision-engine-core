dd24fee22eba86cdb909abc1ce28e4c01769ee35
tstaisyu <t_shiba117@yahoo.co.jp>
2026-05-01 14:16:30 +0900

    Extract state rule evaluation helpers

--- src/evaluate.js
+++ src/evaluate.js
@@ -1,4 +1,5 @@
 const { config: defaultConfig } = require("./config");
+const { matchRule, resolveStateRules } = require("./rules");
 
 function normalizeInput(input) {
   const {
@@ -48,29 +49,6 @@ function normalizeInput(input) {
     previousStateSafe,
     rawStateDurationMs
   };
-}
-
-function matchRule(rule, normalized) {
-  if (rule.type === "value_gte") {
-    return normalized.value >= rule.threshold;
-  }
-
-  if (rule.type === "hysteresis") {
-    return (
-      normalized.previousStateSafe === rule.state &&
-      normalized.value > rule.offThreshold
-    );
-  }
-
-  if (rule.type === "rate_gt") {
-    return normalized.stateRate > rule.threshold;
-  }
-
-  if (rule.type === "rate_lt") {
-    return normalized.stateRate < rule.threshold;
-  }
-
-  return false;
 }
 
 function deriveState(normalized, config) {
@@ -261,88 +239,7 @@ function evaluate(input, config) {
               ? config.states.hot.offThreshold
               : defaultConfig.states.hot.offThreshold
       },
-      rules:
-        config &&
-        config.states &&
-        Array.isArray(config.states.rules)
-          ? config.states.rules
-          : [
-              {
-                name: "critical",
-                type: "value_gte",
-                threshold:
-                  typeof config.criticalThreshold === "number"
-                    ? config.criticalThreshold
-                    : config &&
-                        config.states &&
-                        config.states.critical &&
-                        typeof config.states.critical.threshold === "number"
-                      ? config.states.critical.threshold
-                      : defaultConfig.states.critical.threshold
-              },
-              {
-                name: "hot",
-                type: "value_gte",
-                threshold:
-                  typeof config.hotOnThreshold === "number"
-                    ? config.hotOnThreshold
-                    : config &&
-                        config.states &&
-                        config.states.hot &&
-                        typeof config.states.hot.onThreshold === "number"
-                      ? config.states.hot.onThreshold
-                      : defaultConfig.states.hot.onThreshold
-              },
-              {
-                name: "hot_hysteresis",
-                type: "hysteresis",
-                state: "hot",
-                onThreshold:
-                  typeof config.hotOnThreshold === "number"
-                    ? config.hotOnThreshold
-                    : config &&
-                        config.states &&
-                        config.states.hot &&
-                        typeof config.states.hot.onThreshold === "number"
-                      ? config.states.hot.onThreshold
-                      : defaultConfig.states.hot.onThreshold,
-                offThreshold:
-                  typeof config.hotOffThreshold === "number"
-                    ? config.hotOffThreshold
-                    : config &&
-                        config.states &&
-                        config.states.hot &&
-                        typeof config.states.hot.offThreshold === "number"
-                      ? config.states.hot.offThreshold
-                      : defaultConfig.states.hot.offThreshold
-              },
-              {
-                name: "warming",
-                type: "rate_gt",
-                threshold:
-                  typeof config.warmingRateThreshold === "number"
-                    ? config.warmingRateThreshold
-                    : config &&
-                        config.states &&
-                        config.states.warming &&
-                        typeof config.states.warming.rateThreshold === "number"
-                      ? config.states.warming.rateThreshold
-                      : defaultConfig.states.warming.rateThreshold
-              },
-              {
-                name: "cooling",
-                type: "rate_lt",
-                threshold:
-                  typeof config.coolingRateThreshold === "number"
-                    ? config.coolingRateThreshold
-                    : config &&
-                        config.states &&
-                        config.states.cooling &&
-                        typeof config.states.cooling.rateThreshold === "number"
-                      ? config.states.cooling.rateThreshold
-                      : defaultConfig.states.cooling.rateThreshold
-              }
-            ],
+      rules: resolveStateRules(config, defaultConfig),
       warming: {
         rateThreshold:
           typeof config.warmingRateThreshold === "number"
---
+++ src/rules.js
@@ -0,0 +1,111 @@
+function matchRule(rule, normalized) {
+  if (rule.type === "value_gte") {
+    return normalized.value >= rule.threshold;
+  }
+
+  if (rule.type === "hysteresis") {
+    return (
+      normalized.previousStateSafe === rule.state &&
+      normalized.value > rule.offThreshold
+    );
+  }
+
+  if (rule.type === "rate_gt") {
+    return normalized.stateRate > rule.threshold;
+  }
+
+  if (rule.type === "rate_lt") {
+    return normalized.stateRate < rule.threshold;
+  }
+
+  return false;
+}
+
+function resolveStateRules(config, defaultConfig) {
+  if (config && config.states && Array.isArray(config.states.rules)) {
+    return config.states.rules;
+  }
+
+  return [
+    {
+      name: "critical",
+      type: "value_gte",
+      threshold:
+        typeof config.criticalThreshold === "number"
+          ? config.criticalThreshold
+          : config &&
+              config.states &&
+              config.states.critical &&
+              typeof config.states.critical.threshold === "number"
+            ? config.states.critical.threshold
+            : defaultConfig.states.critical.threshold
+    },
+    {
+      name: "hot",
+      type: "value_gte",
+      threshold:
+        typeof config.hotOnThreshold === "number"
+          ? config.hotOnThreshold
+          : config &&
+              config.states &&
+              config.states.hot &&
+              typeof config.states.hot.onThreshold === "number"
+            ? config.states.hot.onThreshold
+            : defaultConfig.states.hot.onThreshold
+    },
+    {
+      name: "hot_hysteresis",
+      type: "hysteresis",
+      state: "hot",
+      onThreshold:
+        typeof config.hotOnThreshold === "number"
+          ? config.hotOnThreshold
+          : config &&
+              config.states &&
+              config.states.hot &&
+              typeof config.states.hot.onThreshold === "number"
+            ? config.states.hot.onThreshold
+            : defaultConfig.states.hot.onThreshold,
+      offThreshold:
+        typeof config.hotOffThreshold === "number"
+          ? config.hotOffThreshold
+          : config &&
+              config.states &&
+              config.states.hot &&
+              typeof config.states.hot.offThreshold === "number"
+            ? config.states.hot.offThreshold
+            : defaultConfig.states.hot.offThreshold
+    },
+    {
+      name: "warming",
+      type: "rate_gt",
+      threshold:
+        typeof config.warmingRateThreshold === "number"
+          ? config.warmingRateThreshold
+          : config &&
+              config.states &&
+              config.states.warming &&
+              typeof config.states.warming.rateThreshold === "number"
+            ? config.states.warming.rateThreshold
+            : defaultConfig.states.warming.rateThreshold
+    },
+    {
+      name: "cooling",
+      type: "rate_lt",
+      threshold:
+        typeof config.coolingRateThreshold === "number"
+          ? config.coolingRateThreshold
+          : config &&
+              config.states &&
+              config.states.cooling &&
+              typeof config.states.cooling.rateThreshold === "number"
+            ? config.states.cooling.rateThreshold
+            : defaultConfig.states.cooling.rateThreshold
+    }
+  ];
+}
+
+module.exports = {
+  matchRule,
+  resolveStateRules
+};
